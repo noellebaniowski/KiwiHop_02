88927eed46c4686f7ad5fb6aabb15f67eae5860a
noellebaniowski <61165553+noellebaniowski@users.noreply.github.com>
2020-04-06 00:30:28 -0400

    4/6
    trying to add things

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+
+    public Rigidbody2D rb2d;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+    }
+}
